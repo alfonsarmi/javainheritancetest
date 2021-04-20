3f38bef0f5b921b882a1a5accb1fc3760b8976d8
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-20 13:07:15 +0200

    external ineheritance

--- NewPackage/Test2.java
+++ NewPackage/Test2.java
@@ -3,9 +3,10 @@ import NewPackage2.*;
 import net.code.javar.*;
 //import NewPackage2;
 
-public class Test2 {
+public class Test2 extends SuperTest {
 	private void Test()
 	{
-		
+		SuperTest test = new SuperTest();
+		test.Test3();
 	}
 }
